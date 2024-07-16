392b3c4cf93ef133e6d7d1d834a38850bd97be72
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-07-16 22:36:55 +0800

    0716

--- animk_builder/src/main/java/com/mozhimen/animk/builder/impls/AnimatorTranslationXYType.kt
+++ animk_builder/src/main/java/com/mozhimen/animk/builder/impls/AnimatorTranslationXYType.kt
@@ -18,7 +18,7 @@ import java.lang.ref.WeakReference
  * @Date 2022/11/17 23:03
  * @Version 1.0
  */
-open class AnimatorTranslationType : BaseAnimatorType<AnimatorTranslationType, Nothing>(), IAnimViewType<AnimatorTranslationType> {
+open class AnimatorTranslationXYType : BaseAnimatorType<AnimatorTranslationXYType, Nothing>(), IAnimViewType<AnimatorTranslationXYType> {
     private var _fromX = 0f
     private var _toX = 0f
     private var _fromY = 0f
@@ -29,12 +29,12 @@ open class AnimatorTranslationType : BaseAnimatorType<AnimatorTranslationType, N
     private var _isPercentageToY = false
     private var _viewRef: WeakReference<View>? = null
 
-    override fun setViewRef(view: View): AnimatorTranslationType {
+    override fun setViewRef(view: View): AnimatorTranslationXYType {
         _viewRef = WeakReference(view)
         return this
     }
 
-    fun from(vararg directions: EDirection): AnimatorTranslationType {
+    fun from(vararg directions: EDirection): AnimatorTranslationXYType {
         if (directions.isNotEmpty()) {
             _fromY = 0f
             _fromX = _fromY
@@ -68,7 +68,7 @@ open class AnimatorTranslationType : BaseAnimatorType<AnimatorTranslationType, N
         return this
     }
 
-    fun to(vararg directions: EDirection): AnimatorTranslationType {
+    fun to(vararg directions: EDirection): AnimatorTranslationXYType {
         if (directions.isNotEmpty()) {
             _toY = 0f
             _toX = _toY
@@ -102,65 +102,65 @@ open class AnimatorTranslationType : BaseAnimatorType<AnimatorTranslationType, N
         return this
     }
 
-    fun fromX(fromX: Float): AnimatorTranslationType {
+    fun fromX(fromX: Float): AnimatorTranslationXYType {
         fromX(fromX, false)
         return this
     }
 
-    fun toX(toX: Float): AnimatorTranslationType {
+    fun toX(toX: Float): AnimatorTranslationXYType {
         toX(toX, false)
         return this
     }
 
-    fun fromY(fromY: Float): AnimatorTranslationType {
+    fun fromY(fromY: Float): AnimatorTranslationXYType {
         fromY(fromY, false)
         return this
     }
 
-    fun toY(toY: Float): AnimatorTranslationType {
+    fun toY(toY: Float): AnimatorTranslationXYType {
         toY(toY, false)
         return this
     }
 
-    fun fromX(fromX: Int): AnimatorTranslationType {
+    fun fromX(fromX: Int): AnimatorTranslationXYType {
         fromX(fromX.toFloat(), false)
         return this
     }
 
-    fun toX(toX: Int): AnimatorTranslationType {
+    fun toX(toX: Int): AnimatorTranslationXYType {
         toX(toX.toFloat(), false)
         return this
     }
 
-    fun fromY(fromY: Int): AnimatorTranslationType {
+    fun fromY(fromY: Int): AnimatorTranslationXYType {
         fromY(fromY.toFloat(), false)
         return this
     }
 
-    fun toY(toY: Int): AnimatorTranslationType {
+    fun toY(toY: Int): AnimatorTranslationXYType {
         toY(toY.toFloat(), false)
         return this
     }
 
-    fun fromX(fromX: Float, percentage: Boolean): AnimatorTranslationType {
+    fun fromX(fromX: Float, percentage: Boolean): AnimatorTranslationXYType {
         _isPercentageFromX = percentage
         this._fromX = fromX
         return this
     }
 
-    fun toX(toX: Float, percentage: Boolean): AnimatorTranslationType {
+    fun toX(toX: Float, percentage: Boolean): AnimatorTranslationXYType {
         _isPercentageToX = percentage
         this._toX = toX
         return this
     }
 
-    fun fromY(fromY: Float, percentage: Boolean): AnimatorTranslationType {
+    fun fromY(fromY: Float, percentage: Boolean): AnimatorTranslationXYType {
         _isPercentageFromY = percentage
         this._fromY = fromY
         return this
     }
 
-    fun toY(toY: Float, percentage: Boolean): AnimatorTranslationType {
+    fun toY(toY: Float, percentage: Boolean): AnimatorTranslationXYType {
         _isPercentageToY = percentage
         this._toY = toY
         return this
@@ -170,30 +170,30 @@ open class AnimatorTranslationType : BaseAnimatorType<AnimatorTranslationType, N
         val animatorSet = AnimatorSet()
         val translationXProperty =
             if (_isPercentageFromX && _isPercentageToY)
-                object : FloatPropertyCompat<View>(View.TRANSLATION_X.name) {
+                object : FloatPropertyCompat<View>(View.X.name) {
                     override fun setValue(obj: View, value: Float) {
-                        obj.translationX = obj.width * value
+                        obj.x = obj.width * value
                     }
 
                     override operator fun get(obj: View): Float {
-                        return obj.translationX
+                        return obj.x
                     }
                 }
             else
-                View.TRANSLATION_X
+                View.X
         val translationYProperty =
             if (_isPercentageFromY && _isPercentageToY)
-                object : FloatPropertyCompat<View>(View.TRANSLATION_Y.name) {
+                object : FloatPropertyCompat<View>(View.Y.name) {
                     override fun setValue(obj: View, value: Float) {
-                        obj.translationY = obj.height * value
+                        obj.y = obj.height * value
                     }
 
                     override operator fun get(obj: View): Float {
-                        return obj.translationY
+                        return obj.y
                     }
                 }
             else
-                View.TRANSLATION_Y
+                View.Y
         val translationX = if (_viewRef?.get() != null) {
             ObjectAnimator.ofFloat(_viewRef?.get()!!, translationXProperty, _fromX, _toX)
         } else
@@ -222,28 +222,28 @@ open class AnimatorTranslationType : BaseAnimatorType<AnimatorTranslationType, N
     /////////////////////////////////////////////////////////////////////////////
 
     companion object {
-        val FROM_LEFT_SHOW: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val FROM_LEFT_SHOW: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             from(EDirection.LEFT)
         }
-        val FROM_TOP_SHOW: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val FROM_TOP_SHOW: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             from(EDirection.TOP)
         }
-        val FROM_RIGHT_SHOW: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val FROM_RIGHT_SHOW: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             from(EDirection.RIGHT)
         }
-        val FROM_BOTTOM_SHOW: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val FROM_BOTTOM_SHOW: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             from(EDirection.BOTTOM)
         }
-        val TO_LEFT_HIDE: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val TO_LEFT_HIDE: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             to(EDirection.LEFT)
         }
-        val TO_TOP_HIDE: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val TO_TOP_HIDE: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             to(EDirection.TOP)
         }
-        val TO_RIGHT_HIDE: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val TO_RIGHT_HIDE: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             to(EDirection.RIGHT)
         }
-        val TO_BOTTOM_HIDE: AnimatorTranslationType = AnimatorTranslationType().apply {
+        val TO_BOTTOM_HIDE: AnimatorTranslationXYType = AnimatorTranslationXYType().apply {
             to(EDirection.BOTTOM)
         }
     }
--- animk_builder/src/main/java/com/mozhimen/animk/builder/mos/MAnimKConfig.kt
+++ animk_builder/src/main/java/com/mozhimen/animk/builder/mos/MAnimKConfig.kt
@@ -11,15 +11,8 @@ import com.mozhimen.basick.utilk.wrapper.UtilKRes
  * @Version 1.0
  */
 data class MAnimKConfig(
-    var fillBefore: Boolean = DEFAULT_FILLBEFORE,
-    var fillAfter: Boolean = DEFAULT_FILLAFTER,
-    var duration: Long = DEFAULT_DURATION,
-    var interpolator: Interpolator = DEFAULT_INTERPOLATOR
-){
-    companion object {
-        val DEFAULT_FILLBEFORE = false
-        val DEFAULT_FILLAFTER = true
-        val DEFAULT_DURATION = 300L
-        val DEFAULT_INTERPOLATOR: Interpolator = AccelerateDecelerateInterpolator()
-    }
-}
+    var fillBefore: Boolean = false,
+    var fillAfter: Boolean = true,
+    var duration: Long = 300L,
+    var interpolator: Interpolator = AccelerateDecelerateInterpolator()
+)
